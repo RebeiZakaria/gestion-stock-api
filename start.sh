6475110e64e875d851bf33c8ee81a65041423c5a
Rebei Zakaria <zikourebei1003@gmail.com>
2025-07-11 07:32:41 +0100

    Fix vendor path and set public directory as Apache roott

--- start.sh
+++ start.sh
@@ -3,5 +3,13 @@
 # Exécute les migrations s’il y en a
 php artisan migrate --force
 
+php artisan config:clear
+php artisan cache:clear
+php artisan config:cache
+php artisan migrate --force
+php artisan key:generate --force
+chmod -R 775 storage bootstrap/cache
+
+
 # Démarre Apache (important pour Render)
 apache2-foreground
